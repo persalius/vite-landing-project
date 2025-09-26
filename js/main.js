bf11fa238afe5763ed24b67399d03ca4ce0a7a6a
Shevchenko Sergey <persalius@gmail.com>
2025-09-26 13:21:07 +0300

    init

--- js/main.js
+++ js/main.js
@@ -4,5 +4,5 @@ $(document).ready(function () {
   // Весь ваш код jQuery должен быть внутри этой функции
 
   // 1. Выбираем элемент с ID 'greeting' и меняем его текст.
-  $("#greeting").text("Привет от jQuery!");
+  $("#greeting").text("Hello from jQuery!");
 });
