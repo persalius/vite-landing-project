5c2f060049ddf1cb997aea87761525a5f721d509
Shevchenko Sergey <persalius@gmail.com>
2025-09-26 20:49:30 +0300

    ads

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-// import { defineConfig } from "vite";
-// import { resolve } from "path";
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
-// const htmlReplacer = () => {
-//   return {
-//     name: "html-replacer",
-//     transformIndexHtml(html) {
-//       const target = '<div id="template"></div>';
-//       const replacement = '\n<section id="app-root">Section</section>';
-//       return html.replace(target, replacement);
-//     },
-//   };
-// };
+const htmlReplacer = () => {
+  return {
+    name: "html-replacer",
+    transformIndexHtml(html) {
+      const target = '<div id="template"></div>';
+      const replacement = '\n<section id="app-root">Section</section>';
+      return html.replace(target, replacement);
+    },
+  };
+};
 
-// export default defineConfig({
-//   plugins: [htmlReplacer()],
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, "index.html"),
-//         about: resolve(__dirname, "about.html"),
-//       },
-//     },
-//   },
-// });
+export default defineConfig({
+  plugins: [htmlReplacer()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
+});
