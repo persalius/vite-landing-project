11e3ad58ed712bfb85ecb5bf617e7177653a1df3
Shevchenko Sergey <persalius@gmail.com>
2025-09-26 18:46:31 +0300

    init

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
 import "../styles/main.scss";
-
-$(document).ready(function () {
-  // Весь ваш код jQuery должен быть внутри этой функции
-
-  // 1. Выбираем элемент с ID 'greeting' и меняем его текст.
-  $("#greeting").text("Hello from jQuery!");
-});
