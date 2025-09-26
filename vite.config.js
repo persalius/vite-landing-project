1c60e09116a49f9edd2d6b467421a7976ff34991
Shevchenko Sergey <persalius@gmail.com>
2025-09-26 12:41:07 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+const htmlReplacer = () => {
+  return {
+    name: "html-replacer",
+    // Хук 'transformIndexHtml' используется для манипуляций с HTML
+    // до того, как он будет передан в Rollup.
+    transformIndexHtml(html) {
+      // 1. Определяем, что нужно заменить
+      const target = '<div id="template"></div>';
+
+      // 2. Определяем, на что нужно заменить
+      const replacement = '\n<section id="app-root">Section</section>';
+
+      // 3. Выполняем замену
+      return html.replace(target, replacement);
+    },
+  };
+};
+
 export default defineConfig({
+  plugins: [htmlReplacer()],
   build: {
     rollupOptions: {
       input: {
