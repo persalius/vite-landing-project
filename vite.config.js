56d31b68352261a1113bec547874d4aa3a0fbe73
Shevchenko Sergey <persalius@gmail.com>
2025-09-29 12:26:04 +0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import fs from "fs";
 
 const htmlReplacer = () => {
   return {
     name: "html-replacer",
     transformIndexHtml(html, ctx) {
-      const cachedData = fs.readFileSync("./about.html", "utf-8");
       const target = '<div id="template"></div>';
-      const replacement = `\n<section id="app-root">${cachedData}</section>`;
+      const replacement = '\n<section id="app-root">Section</section>';
       return html.replace(target, replacement);
     },
   };
